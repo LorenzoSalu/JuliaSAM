21cae45816e825d44a30424267eb7a3239f15f0c
LorenzoSalu <164194123+LorenzoSalu@users.noreply.github.com>
2025-05-19 19:16:16 +0200

    Implementata la struttura MaskDecoder.
    È stata implementata la struttura MaskDecoder, e relativo forward pass, a chiusura dello sviluppo del file mask_decoder.jl.

--- src/mask_decoder.jl
+++ src/mask_decoder.jl
@@ -57,25 +57,254 @@ function MLP(;
 end
 
 function (self::MLP)(x::AbstractArray)
-	A, B, C, D = size(x)
-	x = sam_reshape(x, (:, D))'
+	if ndims(x) == 4
+		A, B, C, D = size(x)
+		x = sam_reshape(x, (:, D))'
 
-	for i in 1:self.num_layers
-		if i < self.num_layers
-			tmp, _ = self.layers[i](x, self.layers_ps[i], self.layers_st[i])
-			x = relu.(tmp)
-		else
-			x, _ = self.layers[i](x, self.layers_ps[i], self.layers_st[i])
+		for i in 1:self.num_layers
+			if i < self.num_layers
+				tmp, _ = self.layers[i](x, self.layers_ps[i], self.layers_st[i])
+				x = relu.(tmp)
+			else
+				x, _ = self.layers[i](x, self.layers_ps[i], self.layers_st[i])
+			end
+
 		end
 
+		if self.sigmoid_output
+			x = sigmoid.(x)
+		end
+
+		D = size(x, 1)
+		x = sam_reshape(x', (A, B, C, D))
 	end
 
-	if self.sigmoid_output
-		x = sigmoid.(x)
+	if ndims(x) == 2
+		x = x'
+
+		for i in 1:self.num_layers
+			if i < self.num_layers
+				tmp, _ = self.layers[i](x, self.layers_ps[i], self.layers_st[i])
+				x = relu.(tmp)
+			else
+				x, _ = self.layers[i](x, self.layers_ps[i], self.layers_st[i])
+			end
+
+		end
+
+		if self.sigmoid_output
+			x = sigmoid.(x)
+		end
+
+		x = x'
 	end
-
-	D = size(x, 1)
-	x = sam_reshape(x', (A, B, C, D))
-
+	
 	return x
+end
+
+
+########################################################
+# MaskDecoder:
+########################################################
+
+struct MaskDecoder
+	transformer_dim::Int
+	transformer::Any
+	num_multimask_outputs::Int
+	iou_token::Embedding
+	iou_token_ps::NamedTuple
+	iou_token_st::NamedTuple
+	num_mask_tokens::Int
+	mask_tokens::Embedding
+	mask_tokens_ps::NamedTuple
+	mask_tokens_st::NamedTuple
+	output_upscaling::Chain
+	output_upscaling_ps::NamedTuple
+	output_upscaling_st::NamedTuple
+	output_hypernetworks_mlps::Vector{MLP}
+	iou_prediction_head::MLP
+end
+
+function MaskDecoder(;
+	transformer_dim::Int,
+	transformer::Any,
+	num_multimask_outputs::Int = 3,
+	activation::Function = gelu_exact,
+	iou_head_depth::Int = 3,
+	iou_head_hidden_dim::Int = 256,
+)
+
+	rng = Random.MersenneTwister()
+	transformer_dim = transformer_dim
+	transformer = transformer
+	num_multimask_outputs = num_multimask_outputs
+
+	iou_token = Lux.Embedding(1 => transformer_dim)
+	iou_token_ps, iou_token_st = Lux.setup(rng, iou_token)
+
+	num_mask_tokens = num_multimask_outputs + 1
+	mask_tokens = Lux.Embedding(num_mask_tokens => transformer_dim)
+	mask_tokens_ps, mask_tokens_st = Lux.setup(rng, mask_tokens)
+
+	output_upscaling = Chain(
+		ConvTranspose(
+			(2, 2),
+			transformer_dim => transformer_dim ÷ 4;
+			stride = (2, 2),
+            cross_correlation = true
+		),
+		LayerNorm2d(transformer_dim ÷ 4),
+		x -> activation.(x),
+		ConvTranspose(
+			(2, 2),
+			transformer_dim ÷ 4 => transformer_dim ÷ 8;
+			stride = (2, 2),
+            cross_correlation = true
+		),
+		x -> activation.(x),
+	)
+
+	output_upscaling_ps, output_upscaling_st = Lux.setup(rng, output_upscaling)
+
+	output_hypernetworks_mlps =
+		[MLP(
+			input_dim = transformer_dim,
+			hidden_dim = transformer_dim,
+			output_dim = transformer_dim ÷ 8,
+			num_layers = 3,
+		) for i in 1:num_mask_tokens]
+
+	iou_prediction_head = MLP(
+		input_dim = transformer_dim,
+		hidden_dim = iou_head_hidden_dim,
+		output_dim = num_mask_tokens,
+		num_layers = iou_head_depth,
+	)
+
+	return MaskDecoder(
+		transformer_dim,
+		transformer,
+		num_multimask_outputs,
+		iou_token,
+		iou_token_ps,
+		iou_token_st,
+		num_mask_tokens,
+		mask_tokens,
+		mask_tokens_ps,
+		mask_tokens_st,
+		output_upscaling,
+		output_upscaling_ps,
+		output_upscaling_st,
+		output_hypernetworks_mlps,
+		iou_prediction_head,
+	)
+
+end
+
+function (self::MaskDecoder)(;
+	image_embeddings::AbstractArray,
+	image_pe::AbstractArray,
+	sparse_prompt_embeddings::AbstractArray,
+	dense_prompt_embeddings::AbstractArray,
+	multimask_output::Bool,
+)::Tuple{AbstractArray, AbstractArray}
+
+	masks, iou_pred = predict_mask(
+		self = self,
+		image_embeddings = image_embeddings,
+		image_pe = image_pe,
+		sparse_prompt_embeddings = sparse_prompt_embeddings,
+		dense_prompt_embeddings = dense_prompt_embeddings,
+	)
+
+	# Select the correct mask or masks for output
+	if multimask_output
+		mask_slice = 2:size(masks, 2)
+	else
+		mask_slice = 1:1
+	end
+
+	masks = masks[:, mask_slice, :, :]
+	iou_pred = iou_pred[:, mask_slice]
+
+	# Prepare output
+	return masks, iou_pred
+end
+
+function predict_mask(;
+	self::MaskDecoder,
+	image_embeddings::AbstractArray,
+	image_pe::AbstractArray,
+	sparse_prompt_embeddings::AbstractArray,
+	dense_prompt_embeddings::AbstractArray,
+)::Tuple{AbstractArray, AbstractArray}
+
+	# Concatenate output tokens
+	output_tokens =
+		cat(self.iou_token_ps.weight, self.mask_tokens_ps.weight; dims = 2)'
+
+	output_tokens = reshape(output_tokens, (1, size(output_tokens)...))
+	output_tokens = repeat(output_tokens, batch_size, 1, 1)
+
+	tokens = cat(output_tokens, sparse_prompt_embeddings, dims = 2)
+
+	# Expand per-image data in batch direction to be per-mask
+	src = repeat(image_embeddings, size(tokens, 1), 1, 1, 1)
+
+	src = src + dense_prompt_embeddings
+
+	pos_src = repeat(image_pe, size(tokens, 1), 1, 1, 1)
+
+	b, c, h, w = size(src)
+
+	# Run the transformer
+	hs, src = self.transformer(
+		image_embedding = src,
+		image_pe = pos_src,
+		point_embedding = tokens,
+	)
+
+	iou_token_out = hs[:, 1, :]
+	mask_tokens_out = hs[:, 2:(1+self.num_mask_tokens), :]
+
+	# Upscale mask embeddings and predict masks using the mask tokens
+	src = permutedims(src, (1, 3, 2))
+	src = sam_reshape(src, (b, c, h, w))
+
+	upscaled_embedding, _ = self.output_upscaling(
+		permutedims(src, (3, 4, 2, 1)), 
+		self.output_upscaling_ps,
+		self.output_upscaling_st
+		)
+
+	upscaled_embedding = permutedims(upscaled_embedding, (4, 3, 1, 2))
+
+	hyper_in_list::Vector{AbstractArray} = []
+	for i in 1:self.num_mask_tokens
+		push!(
+			hyper_in_list,
+			self.output_hypernetworks_mlps[i](mask_tokens_out[:, i, :]),
+		)
+	end
+
+    hyper_in_list = 
+        [reshape(tensor, size(tensor, 1), 1, size(tensor, 2)) 
+        for tensor in hyper_in_list]
+
+	hyper_in = cat(hyper_in_list...; dims = 2)
+
+	b, c, h, w = size(upscaled_embedding)
+	upscaled_embedding = sam_reshape(upscaled_embedding, (b, c, h * w))
+
+	masks = batched_mul(
+        permutedims(hyper_in, (2, 3, 1)), 
+        permutedims(upscaled_embedding, (2, 3, 1))
+        )
+
+	masks = sam_reshape(masks, (b, :, h, w))
+
+	# Generate mask quality predictions
+	iou_pred = self.iou_prediction_head(iou_token_out)
+
+	return masks, iou_pred
 end
